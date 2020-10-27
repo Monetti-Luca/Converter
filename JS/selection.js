9e1f6842ecf886c6987208f0c3b5530411adac07
Jelox <monettiluca03@gmail.com>
2020-10-27 20:07:00 +0100

    Bin-Int
    Aggiunta la conversione da Binario ad intero
    Bug Fix
    Controllo in input
    Controlli e migliorie minori

---
+++ JS/selection.js
@@ -0,0 +1,72 @@
+//Fuzioni Utility
+function Converti() {
+    var from = document.getElementById("select1").value;
+    var to = document.getElementById("select2").value;
+    
+    var txt = document.getElementById("Input1").value;
+
+    if (from == "Binario") {
+        switch (to){
+            case "Binario": Conv_Same(txt);
+            case "Intero": Conv_Bin_Int(txt);
+        }
+    }
+}
+
+function Reset(){
+    var from = document.getElementById("Input1").value = "";
+    var to = document.getElementById("Input2").value = "";
+}
+
+function Swap() {
+    let sel1 = document.getElementById("select1");
+    let sel2 = document.getElementById("select2");
+    let temp = sel1.selectedIndex;
+
+    sel1.selectedIndex = sel2.selectedIndex;
+    sel2.selectedIndex = temp;
+
+    let input1 = document.getElementById("Input1");
+    let input2 = document.getElementById("Input2");
+    let temp1 = input1.value;
+
+    input1.value = input2.value;
+    input2.value = temp1;
+}
+
+//Funzioni Di controllo Input
+function Control(input) {
+    if (document.getElementById("select1").value == "Binario"){
+        let regex = /[^01 ]/gi;
+        input.value = input.value.replace(regex, "");
+    }
+}
+
+
+
+//Funzioni di conversione
+
+
+function Conv_Bin_Int(txt){
+    var arr = [];
+    arr = txt.split(" ");
+    console.log(arr);
+    conversione = ""
+
+    for (let j = 0; j < arr.length; j++){
+        if (arr[j] == ""){
+            continue;
+        }
+        let bin = arr[j].split("").map(function(n) { return Number(n)});
+
+        let intero = 0;
+
+        for (i = 0; i < bin.length; i++) {
+            intero += bin[bin.length - 1 - i] * Math.pow(2, i);
+        }
+
+        conversione += String(intero) + " ";
+    }
+
+    document.getElementById("Input2").value = conversione;
+}
