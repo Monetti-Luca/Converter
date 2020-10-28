91f806e127f7e761235d4692c45299262deb50b5
Jelox <monettiluca03@gmail.com>
2020-10-28 18:18:09 +0100

    Javascript Update
    Aggiunta La conversione da Esadecimale a Decimale
    Aggiunta la conversione da decimale a Esadecimale
    Aggiornato il controllo per l'input

--- JS/selection.js
+++ JS/selection.js
@@ -15,6 +15,12 @@ function Converti() {
         switch (to) {
             case "Intero": Conv_Same(txt);
             case "Binario": Conv_Int_Bin(txt);
+            case "Esadecimale": Conv_Int_Esa(txt);
+        }
+    }
+    if (from == "Esadecimale") {
+        switch (to) {
+            case "Intero": Conv_Esa_Int(txt);
         }
     }
 }
@@ -40,10 +46,47 @@ function Swap() {
     input2.value = temp1;
 }
 
+function Esa_Int(chr) {
+    if (chr >= "0" && chr <= "9" || chr >= "A" && chr <= "F") {
+        switch (chr) {
+            case "A": return 10;
+            case "B": return 11;
+            case "C": return 12;
+            case "D": return 13;
+            case "E": return 14;
+            case "F": return 15;
+            default : return Number(chr);
+        }
+    }
+}
+function Int_Esa(n) {
+    if (n >= 0 && n < 16) {
+        switch (n) {
+            case 10: return "A";
+            case 11: return "B";
+            case 12: return "C";
+            case 13: return "D";
+            case 14: return "E";
+            case 15: return "F";
+            default: return n;
+        }
+    }
+}
+
 //Funzioni Di controllo Input
 function Control(input) {
-    if (document.getElementById("select1").value == "Binario"){
+    let doc = document.getElementById("select1").value;
+
+    if (doc == "Binario") {
         let regex = /[^01 ]/gi;
+        input.value = input.value.replace(regex, "");
+    }
+    if (doc == "Intero") {
+        let regex = /[^0-9 ]/gi;
+        input.value = input.value.replace(regex, "");
+    }
+    if (doc == "Esadecimale") {
+        let regex = /[^0-9ABCDEF ]/g;
         input.value = input.value.replace(regex, "");
     }
 }
@@ -99,4 +142,49 @@ function Conv_Int_Bin(txt) {
     }
 
     document.getElementById("Input2").value = conversione;
+}
+
+
+function Conv_Esa_Int(txt) {
+    let arr = [];
+    arr = txt.split(" ");
+    let conversione = "";
+
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] == "") continue;
+
+        let esa = arr[j].split("");
+        let intero = 0;
+
+        for (let i = 0; i < esa.length; i++) {
+            intero += Esa_Int(esa[esa.length - i - 1]) * Math.pow(16, i);
+        }
+
+        conversione += intero + " ";
+    }
+
+    document.getElementById("Input2").value = conversione;
+}
+
+function Conv_Int_Esa(txt) {
+    let arr = [];
+    arr = txt.split(" ");
+    let conversione = "";
+
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] == "") continue;
+
+        let int = Number(arr[j]);
+
+        let esa = Esa_Ric(int);
+
+        conversione += esa + " ";
+    }
+
+    document.getElementById("Input2").value = conversione;
+}
+
+function Esa_Ric(n) {
+    if (n < 16) return String(Int_Esa(n));
+    else return Esa_Ric(Math.floor(n / 16)) + Esa_Ric(n % 16);
 }
